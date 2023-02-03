6eda4542d4f6f9dafc9dd2a9c4fe4c5f42f8f494
Andrea Longoni <andrealong2000@gmail.com>
2023-02-03 18:52:26 +0100

    Add datasets generator

--- src/generator.py
+++ src/generator.py
@@ -127,41 +127,6 @@ def generate_transactions_table(customer_profile, start_date = "2018-04-01", nb_
     return customer_transactions  
     
 
-def generate_dataset(n_customers = 10000, n_terminals = 1000000, nb_days=90, start_date="2018-04-01", r=5):
-    
-    start_time=time.time()
-    customer_profiles_table = generate_customer_profiles_table(n_customers, random_state = 0)
-    print("Time to generate customer profiles table: {0:.2}s".format(time.time()-start_time))
-    
-    start_time=time.time()
-    terminal_profiles_table = generate_terminal_profiles_table(n_terminals, random_state = 1)
-    print("Time to generate terminal profiles table: {0:.2}s".format(time.time()-start_time))
-    
-    start_time=time.time()
-    x_y_terminals = terminal_profiles_table[['x_terminal_id','y_terminal_id']].values.astype(float)
-    customer_profiles_table['available_terminals'] = customer_profiles_table.apply(lambda x : get_list_terminals_within_radius(x, x_y_terminals=x_y_terminals, r=r), axis=1)
-    # With Pandarallel
-    #customer_profiles_table['available_terminals'] = customer_profiles_table.parallel_apply(lambda x : get_list_closest_terminals(x, x_y_terminals=x_y_terminals, r=r), axis=1)
-    customer_profiles_table['nb_terminals']=customer_profiles_table.available_terminals.apply(len)
-    print("Time to associate terminals to customers: {0:.2}s".format(time.time()-start_time))
-    
-    start_time=time.time()
-    transactions_df=customer_profiles_table.groupby('CUSTOMER_ID').apply(lambda x : generate_transactions_table(x.iloc[0], nb_days=nb_days)).reset_index(drop=True)
-    # With Pandarallel
-    #transactions_df=customer_profiles_table.groupby('CUSTOMER_ID').parallel_apply(lambda x : generate_transactions_table(x.iloc[0], nb_days=nb_days)).reset_index(drop=True)
-    print("Time to generate transactions: {0:.2}s".format(time.time()-start_time))
-    
-    # Sort transactions chronologically
-    transactions_df=transactions_df.sort_values('TX_DATETIME')
-    # Reset indices, starting from 0
-    transactions_df.reset_index(inplace=True,drop=True)
-    transactions_df.reset_index(inplace=True)
-    # TRANSACTION_ID are the dataframe indices, starting from 0
-    transactions_df.rename(columns = {'index':'TRANSACTION_ID'}, inplace = True)
-    
-    return (customer_profiles_table, terminal_profiles_table, transactions_df)
-
-
 def add_frauds(customer_profiles_table, terminal_profiles_table, transactions_df):
     
     # By default, all transactions are genuine
@@ -169,12 +134,15 @@ def add_frauds(customer_profiles_table, terminal_profiles_table, transactions_df
     transactions_df['TX_FRAUD_SCENARIO']=0
     
     # Scenario 1
+    start_time=time.time()
     transactions_df.loc[transactions_df.TX_AMOUNT>220, 'TX_FRAUD']=1
     transactions_df.loc[transactions_df.TX_AMOUNT>220, 'TX_FRAUD_SCENARIO']=1
     nb_frauds_scenario_1=transactions_df.TX_FRAUD.sum()
+    print("Time to generate frauds from scenario 1:  {0:>8.2f}s".format(time.time()-start_time))
     print("Number of frauds from scenario 1: "+str(nb_frauds_scenario_1))
     
     # Scenario 2
+    start_time=time.time()
     for day in range(transactions_df.TX_TIME_DAYS.max()):
         
         compromised_terminals = terminal_profiles_table.TERMINAL_ID.sample(n=2, random_state=day)
@@ -187,9 +155,11 @@ def add_frauds(customer_profiles_table, terminal_profiles_table, transactions_df
         transactions_df.loc[compromised_transactions.index,'TX_FRAUD_SCENARIO']=2
     
     nb_frauds_scenario_2=transactions_df.TX_FRAUD.sum()-nb_frauds_scenario_1
+    print("Time to generate frauds from scenario 2:  {0:>8.2f}s".format(time.time()-start_time))
     print("Number of frauds from scenario 2: "+str(nb_frauds_scenario_2))
     
     # Scenario 3
+    start_time=time.time()
     for day in range(transactions_df.TX_TIME_DAYS.max()):
         
         compromised_customers = customer_profiles_table.CUSTOMER_ID.sample(n=3, random_state=day).values
@@ -210,6 +180,44 @@ def add_frauds(customer_profiles_table, terminal_profiles_table, transactions_df
         
                              
     nb_frauds_scenario_3=transactions_df.TX_FRAUD.sum()-nb_frauds_scenario_2-nb_frauds_scenario_1
+    print("Time to generate frauds from scenario 3:  {0:>8.2f}s".format(time.time()-start_time))
     print("Number of frauds from scenario 3: "+str(nb_frauds_scenario_3))
     
     return transactions_df
+
+
+def generate_dataset(n_customers = 10000, n_terminals = 1000000, nb_days=90, start_date="2018-04-01", r=5):
+    
+    start_time=time.time()
+    customer_profiles_table = generate_customer_profiles_table(n_customers, random_state = 0)
+    print("Time to generate customer profiles table: {0:>8.2f}s".format(time.time()-start_time))
+    
+    start_time=time.time()
+    terminal_profiles_table = generate_terminal_profiles_table(n_terminals, random_state = 1)
+    print("Time to generate terminal profiles table: {0:>8.2f}s".format(time.time()-start_time))
+    
+    start_time=time.time()
+    x_y_terminals = terminal_profiles_table[['x_terminal_id','y_terminal_id']].values.astype(float)
+    customer_profiles_table['available_terminals'] = customer_profiles_table.apply(lambda x : get_list_terminals_within_radius(x, x_y_terminals=x_y_terminals, r=r), axis=1)
+    # With Pandarallel
+    #customer_profiles_table['available_terminals'] = customer_profiles_table.parallel_apply(lambda x : get_list_closest_terminals(x, x_y_terminals=x_y_terminals, r=r), axis=1)
+    customer_profiles_table['nb_terminals']=customer_profiles_table.available_terminals.apply(len)
+    print("Time to associate terminals to customers: {0:>8.2f}s".format(time.time()-start_time))
+    
+    start_time=time.time()
+    transactions_df=customer_profiles_table.groupby('CUSTOMER_ID').apply(lambda x : generate_transactions_table(x.iloc[0], nb_days=nb_days)).reset_index(drop=True)
+    # With Pandarallel
+    #transactions_df=customer_profiles_table.groupby('CUSTOMER_ID').parallel_apply(lambda x : generate_transactions_table(x.iloc[0], nb_days=nb_days)).reset_index(drop=True)
+    print("Time to generate transactions:            {0:>8.2f}s".format(time.time()-start_time))
+    
+    # Sort transactions chronologically
+    transactions_df=transactions_df.sort_values('TX_DATETIME')
+    # Reset indices, starting from 0
+    transactions_df.reset_index(inplace=True,drop=True)
+    transactions_df.reset_index(inplace=True)
+    # TRANSACTION_ID are the dataframe indices, starting from 0
+    transactions_df.rename(columns = {'index':'TRANSACTION_ID'}, inplace = True)
+
+    transactions_df = add_frauds(customer_profiles_table, terminal_profiles_table, transactions_df)
+    
+    return (customer_profiles_table, terminal_profiles_table, transactions_df)
