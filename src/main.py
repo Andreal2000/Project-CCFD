8e6667acddc2b9ef811ac5b5cdcecc4eca87eaf2
Andrea Longoni <andrealong2000@gmail.com>
2023-03-10 17:56:09 +0100

    Update datasets generator

--- src/main.py
+++ src/main.py
@@ -4,17 +4,17 @@ import pandas as pd
 from database import Database
 from generator import generate_dataset
 
-DIR_DATA = "./data/"
+DIR_DATA = "./data"
 
 def generate_all_datasets(force=False):
-    dataset_template = {100: ( 5500, 10000, 180),
-                        200: (10000, 20000, 180),
-                        300: (15000, 30000, 180),}
-    
+    dataset_template = {100: (17500, 35000, 10),
+                        200: (25500, 50000, 10),
+                        300: (35500, 55000, 10),}
+
     file_names = ("customer", "terminal", "transaction")
 
     for k, v in dataset_template.items():
-        path = f"{DIR_DATA}{k}/"
+        path = f"{DIR_DATA}/{k}/"
 
         if force or not any([os.path.isfile(path+f"{name}.pkl") for name in file_names]):
             print(f"Generate {k}Mbyte dataset")
@@ -25,7 +25,7 @@ def generate_all_datasets(force=False):
                                                  nb_days     = v[2], 
                                                  start_date  = "2018-04-01",
                                                  r           = 5)))
-            
+
             if not os.path.exists(path):
                 os.makedirs(path)
 
@@ -57,7 +57,7 @@ def generate_test_dataset(force=False):
 
 if __name__ == "__main__":
     generate_all_datasets()
-    generate_test_dataset()
+    # generate_test_dataset()
 
     # See https://neo4j.com/developer/aura-connect-driver/ for Aura specific connection URL.
     scheme = "neo4j"  # Connecting to Aura, use the "neo4j+s" or "neo4j+ssc" URI scheme
@@ -66,27 +66,28 @@ if __name__ == "__main__":
     url = f"{scheme}://{host_name}:{port}"
     user = "<Username for Neo4j database>"
     password = "<Password for Neo4j database>"
-    db = Database(url, user, password)
-    try:
-        customer_df = pd.read_pickle(f"{DIR_DATA}test/customer.pkl")
-        db.insert_customer(customer_df) # 13214 ms
-        
-        terminal_df = pd.read_pickle(f"{DIR_DATA}test/terminal.pkl")
-        db.insert_terminal(terminal_df) # 7513 ms
-        
-        transaction_df = pd.read_pickle(f"{DIR_DATA}test/transaction.pkl")
-        db.insert_transaction(transaction_df)
-        
-        # db.create_friendship("Alice", "David")
-        # db.find_person("Alice")
-    finally:
-        db.close()
+    # db = Database(url, user, password)
+    # try:
+    #     customer_df = pd.read_pickle(f"{DIR_DATA}/test/customer.pkl")
+    #     db.insert_customer(customer_df) # 13214 ms
 
+    #     terminal_df = pd.read_pickle(f"{DIR_DATA}/test/terminal.pkl")
+    #     db.insert_terminal(terminal_df) # 7513 ms
 
-    # customer_df = pd.read_pickle(f"{DIR_DATA}test/customer.pkl")
-    # terminal_df = pd.read_pickle(f"{DIR_DATA}test/terminal.pkl")
-    # transaction_df = pd.read_pickle(f"{DIR_DATA}test/transaction.pkl")
+    #     transaction_df = pd.read_pickle(f"{DIR_DATA}/test/transaction.pkl")
+    #     db.insert_transaction(transaction_df)
 
-    # print(customer_df)
-    # print(terminal_df)
-    # print(transaction_df)
+    #     # db.create_friendship("Alice", "David")
+    #     # db.find_person("Alice")
+    # finally:
+    #     db.close()
+
+    for i in range(100, 301, 100):
+        print(i, "Mbyte dataset")
+        customer_df = pd.read_pickle(f"{DIR_DATA}/{i}/customer.pkl")
+        terminal_df = pd.read_pickle(f"{DIR_DATA}/{i}/terminal.pkl")
+        transaction_df = pd.read_pickle(f"{DIR_DATA}/{i}/transaction.pkl")
+
+        print(customer_df.shape)
+        print(terminal_df.shape)
+        print(transaction_df.shape)
